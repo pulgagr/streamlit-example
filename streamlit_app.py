3c068962b4b57bfd3a686b9752dd8055f44646b6
pulgagr <pulgagr@gmail.com>
2023-11-08 16:43:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,40 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
 import streamlit as st
+import os
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+text =""
+os.environ['OPENAI_API_KEY'] = st.secrets['apikey']
+chain = load_qa_chain(OpenAI(),chain_type='stuff')
+st.set_page_config(page_title='DOCGPT')
+st.header('DocGPT :books:')
+prompt = st.text_input("Ask a question about your document:")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+text_splitter =CharacterTextSplitter(
+    separator='\n',
+    chunk_size=800,
+    chunk_overlap=200,
+    length_function=len,
+)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+embeddings = OpenAIEmbeddings()
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+with st.sidebar:
+    st.subheader("Your Document")
+    pdf_doc = st.file_uploader('Upload your PDF')
+    if st.button('Process'):
+        with st.spinner("Processing"):
+            pdf_reader = PdfReader(pdf_doc)
+            for page in pdf_reader.pages:
+                text+=page.extract_text()
+            texts = text_splitter.split_text(text)
+            document_search = FAISS.from_texts(texts,embeddings)
+            if prompt:
+                docs = document_search.similarity_search(prompt)
+                st.write(chain.run(input_documents=docs,question=prompt))
